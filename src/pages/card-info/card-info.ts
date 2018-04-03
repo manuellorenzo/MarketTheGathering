1930745eb7f70223553235ddfc01a4de7dea5a5e
Manuel <manuel.lorenzo@optimaonline.es>
2018-04-03 14:59:01 +0200

    Increase all cards list performance and added button to show the double-faced cards

--- src/pages/card-info/card-info.ts
+++ src/pages/card-info/card-info.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ApiScryfallProvider } from '../../providers/api-scryfall/api-scryfall';
 
 import { Observable } from 'rxjs/Observable';
@@ -23,30 +23,62 @@ import 'rxjs/add/observable/throw';
 export class CardInfoPage {
 
   private cardId: string;
-  private cardInfo = {name: "1"};
+  private cardInfo = {};
+  private loadComplete: boolean;
+  private loading;
+  private sideShown:boolean;
+  private imagenLoaded:boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _apiScryfallProvider: ApiScryfallProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _apiScryfallProvider: ApiScryfallProvider, public loadingCtrl: LoadingController) {
   }
 
   ngOnInit() {
+    this.imagenLoaded = false;
+    this.sideShown = true;
+    this.loadComplete = false;
     this.cardId = this.navParams.get('cardId');
+    this.createLoader();
+    this.presentLoadingDefault();
     this.getCardById();
   }
 
   getCardById() {
     this._apiScryfallProvider.getCardById(this.cardId).pipe(
       map((card: any) => {
-
-        if (card.image_uris != undefined && card.image_uris != null) {
-        } else if (card.card_faces != undefined && card.card_faces != null) {
-        }
         this.cardInfo = card;
         console.log(this.cardInfo);
       })
     ).catch((error) => {
       console.log(error);
       return Observable.throw(error);
+    }).finally(() => {
+      this.loadComplete = true;
+      this.loading.dismiss();
     }).subscribe();
   }
 
+  createLoader() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Cargando cartas...'
+    });
+  }
+
+  presentLoadingDefault() {
+    this.loading.present();
+  }
+
+  dismissLoading() {
+    if (this.loading) {
+      this.loading.dismiss();
+      this.loading = null;
+    }
+  }
+
+  changeSide(){
+    this.sideShown = !this.sideShown;
+  }
+
+  changeLoaded(){
+    this.imagenLoaded = !this.imagenLoaded;
+  }
 }
--- src/pages/listAllCards/listAllCards.ts
+++ src/pages/listAllCards/listAllCards.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, LoadingController, Content, Img } from 'ionic-angular';
 
 import { ApiScryfallProvider } from '../../providers/api-scryfall/api-scryfall';
 import { CardInfoPage } from '../card-info/card-info';
 
+import { Observable } from 'rxjs/Observable';
 import { map, concatMap } from 'rxjs/operators';
 import 'rxjs/add/operator/finally';
+import { updateImgs } from 'ionic-angular/components/content/content';
 
 @Component({
   selector: 'listAllCards-page',
   templateUrl: 'listAllCards.html'
 })
 export class listAllCardsPage {
+    //Workaround done here because ion-img was not working with the virtual list
+    //More info here https://github.com/ionic-team/ionic/issues/9660#issuecomment-304840427
+    @ViewChild(Content) _content: Content;
+    ngAfterViewInit(){
+      if(this._content) {
+        this._content.imgsUpdate = () => {
+          if (this._content._scroll.initialized && this._content._imgs.length && this._content.isImgsUpdatable()) {
+            // reset cached bounds
+            this._content._imgs.forEach((img:Img)=>img._rect = null);
+            // use global position to calculate if an img is in the viewable area
+            updateImgs(this._content._imgs, this._content._cTop * -1, this._content.contentHeight, this._content.directionY, 1400, 400);
+          }
+        };
+      }
+    }
+
   selectedItem: any;
   icons: string[];
   items: Array<{ name: string, frontImage: string, backImage: string, cardId: string }>;
@@ -81,6 +99,9 @@ export class listAllCardsPage {
       if (infiniteScroll != null) {
         infiniteScroll.complete();
       }
+    }).catch((error)=>{
+      console.log(error);
+      return Observable.throw(error);
     }).subscribe();
   }
 
